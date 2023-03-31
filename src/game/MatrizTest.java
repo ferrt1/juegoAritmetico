cb99ddee401f72cecc65a71fd6fb9aedf0887f95
Fabricio Gordillo <fabrig.1209@gmail.com>
2023-03-31 15:22:58 -0300

    Se agregaron algunos tests

--- src/game/MatrizTest.java
+++ src/game/MatrizTest.java
@@ -8,19 +8,19 @@ import org.junit.Test;
 public class MatrizTest {
 	
 	Matriz sistema;
+	int[][] matrizTestIncompleta = new int [1][4];
 
 	@Before
 	public void setUp() {
 		sistema = new Matriz();
-		
+		matrizTestIncompleta[0][0] = 1; matrizTestIncompleta[0][2] = 4; matrizTestIncompleta[0][3] = 2;
 	}
 	
 	@Test
 	public void SumaFilaTest() {
-		int[][] matrizTest = new int [1][4];
-		matrizTest[0][0] = 1; matrizTest[0][1] = 5; matrizTest[0][2] = 4; matrizTest[0][3] = 2;
+		matrizTestIncompleta[0][1] = 5;
 		
-		assertTrue(sistema.verificarSumaFila(matrizTest, 0, 12));
+		assertTrue(sistema.verificarSumaFila(matrizTestIncompleta, 0, 12));
 	}
 	
 	@Test
@@ -31,4 +31,31 @@ public class MatrizTest {
 		assertTrue(sistema.verificarSumaColumna(matrizTest, 0, 13));
 	}
 	
+	@Test(expected=IllegalArgumentException.class)
+	public void noSePermitenLetras() {
+		matrizTestIncompleta[0][1] = 'e';
+		
+		sistema.verificarSumaFila(matrizTestIncompleta, 0, 7);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void noSePermitenNegativos() {
+		matrizTestIncompleta[0][1] = -1;
+		
+		sistema.verificarSumaFila(matrizTestIncompleta, 0, 6);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void noSePermitenCeros() {
+		matrizTestIncompleta[0][1] = 0;
+		
+		sistema.verificarSumaFila(matrizTestIncompleta, 0, 7);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void hayEspaciosVacios() {
+		matrizTestIncompleta[0][1] = ' ';
+		
+		sistema.verificarSumaFila(matrizTestIncompleta, 0, 7);
+	}
 }
