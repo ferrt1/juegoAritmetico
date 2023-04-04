e12c325f51b65ac3dd74cf89ba0d1340cf1e73f7
Fernando <fernandotrejo125@gmail.com>
2023-04-04 07:04:40 -0300

    Agregando niveles 2

--- src/game/Interfaz.java
+++ src/game/Interfaz.java
@@ -14,6 +14,7 @@ import javax.swing.JTextField;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.awt.Font;
 import java.awt.Color;
@@ -27,8 +28,15 @@ import javax.swing.border.LineBorder;
 public class Interfaz {
 
 	private JFrame frmJuegoAritmetico;
-	private Matriz matriz;
 	private int[][] matrizRecibida;
+	private Matriz matriz = new Matriz();
+	private Niveles nivelClase = new Niveles();
+	private static int nivel = 1;
+	
+	//	Listas para poder acceder a cada elemento sin perder su posicion
+	private LinkedList<JTextField> dataTextFieldLista = new LinkedList<JTextField>();
+	private LinkedList<JLabel> arregloSumaFila = new LinkedList<JLabel>();
+	private LinkedList<JLabel> arregloSumaColumna = new LinkedList<JLabel>();
 	
 	/**
 	 * Launch the application.
@@ -52,19 +60,62 @@ public class Interfaz {
 	public Interfaz() {
 		initialize();
 	}
+	
 
+	
+	public void createPanelHorizontal(int nivelActual) {
+		JPanel panelArregloHorizontal = new JPanel();
+		
+		panelArregloHorizontal.setForeground(new Color(251, 194, 252));
+		panelArregloHorizontal.setBorder(new LineBorder(new Color(251, 194, 252), 3, true));
+		panelArregloHorizontal.setBackground(new Color(251, 194, 252));
+		panelArregloHorizontal.setBounds(80, 362, 446, 60);
+		frmJuegoAritmetico.getContentPane().add(panelArregloHorizontal);
+		panelArregloHorizontal.setLayout(new GridLayout(1, 0, 4, 4));
+		
+		for(int i = 0; i<4; i++) {
+			JLabel jLabelHorizontal = new JLabel();
+			jLabelHorizontal.setFont(new Font("Microsoft New Tai Lue", Font.BOLD, 26));
+			jLabelHorizontal.setBackground(new Color(53, 28, 77));
+			jLabelHorizontal.setForeground(new Color(251, 194, 252));
+			jLabelHorizontal.setHorizontalAlignment(SwingConstants.CENTER);
+			panelArregloHorizontal.add(jLabelHorizontal);
+			jLabelHorizontal.setOpaque(true);
+			jLabelHorizontal.setText(Integer.toString(nivelClase.cambiarNivelColumna(nivelActual)[i]));
+			arregloSumaColumna.add(jLabelHorizontal);
+		}
+		
+	}
+	
+	public void createPanelVertical(int nivelActual) {
+		JPanel panelArregloVertical = new JPanel();
+		panelArregloVertical.setBorder(new LineBorder(new Color(251, 194, 252), 3, true));
+		panelArregloVertical.setBackground(new Color(251, 194, 252));
+		panelArregloVertical.setBounds(560, 35, 80, 302);
+		frmJuegoAritmetico.getContentPane().add(panelArregloVertical);
+		panelArregloVertical.setLayout(new GridLayout(0, 1, 4, 4));
+
+		for(int i = 0; i< 4; i++) {
+			JLabel jLabelVertical= new JLabel();
+			jLabelVertical.setFont(new Font("Microsoft New Tai Lue", Font.BOLD, 26));
+			jLabelVertical.setBackground(new Color(53, 28, 77));
+			jLabelVertical.setForeground(new Color(251, 194, 252));
+			jLabelVertical.setHorizontalAlignment(SwingConstants.CENTER);
+			jLabelVertical.setOpaque(true);
+			panelArregloVertical.add(jLabelVertical);
+			jLabelVertical.setText(Integer.toString(nivelClase.cambiarNivelFila(nivel)[i]));
+			arregloSumaFila.add(jLabelVertical);
+		}
+	}
+	
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
 
-		Matriz matriz = new Matriz();
+		matrizRecibida = new int[4][4];
 
-		LinkedList<JTextField> dataTextFieldLista = new LinkedList<JTextField>();
-		LinkedList<JLabel> arrayLabelHorizontal = new LinkedList<JLabel>();
-		LinkedList<JLabel> arrayLabelVertical = new LinkedList<JLabel>();
-
-		
 		frmJuegoAritmetico = new JFrame();
 		frmJuegoAritmetico.getContentPane().setForeground(new Color(251, 194, 252));
 		frmJuegoAritmetico.setTitle("Juego Aritmetico");
@@ -75,6 +126,8 @@ public class Interfaz {
 		frmJuegoAritmetico.setBounds(100, 100, 765, 561);
 		frmJuegoAritmetico.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmJuegoAritmetico.getContentPane().setLayout(null);
+
+		//		Panel de la matriz del juego
 
 		JPanel panelMatriz = new JPanel();
 		panelMatriz.setBorder(new LineBorder(new Color(251, 194, 252), 2, true));
@@ -96,46 +149,14 @@ public class Interfaz {
 		}
 
 
-		JPanel panelArregloVertical = new JPanel();
-		panelArregloVertical.setBorder(new LineBorder(new Color(251, 194, 252), 3, true));
-		panelArregloVertical.setBackground(new Color(251, 194, 252));
-		panelArregloVertical.setBounds(560, 35, 80, 302);
-		frmJuegoAritmetico.getContentPane().add(panelArregloVertical);
-		panelArregloVertical.setLayout(new GridLayout(0, 1, 4, 4));
+		//		Agrego el arreglo donde se encuentra el resultado que debo sumar verticalmente
+		//		Agrego el arreglo donde se encuentra el resultado que debo sumar horizontalmente
 
-		for(int i = 0; i< 4; i++) {
-			JLabel jLabelVertical= new JLabel();
-			jLabelVertical.setFont(new Font("Microsoft New Tai Lue", Font.BOLD, 26));
-			jLabelVertical.setBackground(new Color(53, 28, 77));
-			jLabelVertical.setForeground(new Color(251, 194, 252));
-			jLabelVertical.setHorizontalAlignment(SwingConstants.CENTER);
-			jLabelVertical.setOpaque(true);
-			panelArregloVertical.add(jLabelVertical);
-			jLabelVertical.setText(Integer.toString(matriz.arregloArmadoFilas[i]));
-		}
+		createPanelHorizontal(nivel);
+		createPanelVertical(nivel);
 
 
-		JPanel panelArregloHorizontal = new JPanel();
-		panelArregloHorizontal.setForeground(new Color(251, 194, 252));
-		panelArregloHorizontal.setBorder(new LineBorder(new Color(251, 194, 252), 3, true));
-		panelArregloHorizontal.setBackground(new Color(251, 194, 252));
-		panelArregloHorizontal.setBounds(80, 362, 446, 60);
-		frmJuegoAritmetico.getContentPane().add(panelArregloHorizontal);
-		panelArregloHorizontal.setLayout(new GridLayout(1, 0, 4, 4));
-
-		
-		for(int i = 0; i<4; i++) {
-			JLabel jLabelHorizontal = new JLabel();
-			jLabelHorizontal.setFont(new Font("Microsoft New Tai Lue", Font.BOLD, 26));
-			jLabelHorizontal.setBackground(new Color(53, 28, 77));
-			jLabelHorizontal.setForeground(new Color(251, 194, 252));
-			jLabelHorizontal.setHorizontalAlignment(SwingConstants.CENTER);
-			panelArregloHorizontal.add(jLabelHorizontal);
-			jLabelHorizontal.setOpaque(true);
-			
-			jLabelHorizontal.setText(Integer.toString(matriz.arregloArmadoColumnas[i]));
-		}
-
+		//		Creacion de boton para Comprobar el estado
 
 		JButton btnComprobar = new JButton("COMPROBAR");
 		btnComprobar.setForeground(new Color(255, 255, 255));
@@ -146,79 +167,210 @@ public class Interfaz {
 		btnComprobar.setContentAreaFilled(true);
 		btnComprobar.setBorderPainted(false);
 		btnComprobar.setFocusPainted(false);
+		frmJuegoAritmetico.getContentPane().add(btnComprobar);
+
+		JButton btnReintentar = new JButton("REINTENTAR");
+		btnReintentar.setOpaque(true);
+		btnReintentar.setForeground(Color.WHITE);
+		btnReintentar.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
+		btnReintentar.setFocusPainted(false);
+		btnReintentar.setContentAreaFilled(true);
+		btnReintentar.setBorderPainted(false);
+		btnReintentar.setBackground(new Color(118, 82, 133));
+		btnReintentar.setBounds(202, 433, 201, 70);
+		frmJuegoAritmetico.getContentPane().add(btnReintentar);
+
+		JButton btnFinalizar = new JButton("FINALIZAR");
+		btnFinalizar.setOpaque(true);
+		btnFinalizar.setForeground(Color.WHITE);
+		btnFinalizar.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
+		btnFinalizar.setFocusPainted(false);
+		btnFinalizar.setContentAreaFilled(true);
+		btnFinalizar.setBorderPainted(false);
+		btnFinalizar.setBackground(new Color(118, 82, 133));
+		btnFinalizar.setBounds(202, 433, 201, 70);
+		frmJuegoAritmetico.getContentPane().add(btnFinalizar);
+		
+		JButton btnSiguiente = new JButton("SIGUIENTE");
+		btnSiguiente.setOpaque(true);
+		btnSiguiente.setForeground(Color.WHITE);
+		btnSiguiente.setFont(new Font("Trebuchet MS", Font.BOLD, 20));
+		btnSiguiente.setFocusPainted(false);
+		btnSiguiente.setContentAreaFilled(true);
+		btnSiguiente.setBorderPainted(false);
+		btnSiguiente.setBackground(new Color(118, 82, 133));
+		btnSiguiente.setBounds(202, 433, 201, 70);
+		frmJuegoAritmetico.getContentPane().add(btnSiguiente);
 
 		btnComprobar.addActionListener(new ActionListener() {
-
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				matrizRecibida = new int[4][4];
+				boolean comprobar = true;
 				try {
 					matriz.agregarNumerosDeListaAMatriz(matrizRecibida, dataTextFieldLista);
+
+					//Oculto el boton cuando comprueba los resultados
+					btnComprobar.setVisible(false);
+					for(int i = 0; i < 16; i++) {
+						dataTextFieldLista.get(i).setEditable(false);
+					}
 				} catch (Exception e2) {
 					JOptionPane.showMessageDialog(new JFrame(), 
-							"La matriz no puede tener espacios vacios o letras");
+							"La matriz no puede tener espacios vacios, letras o ceros");
+					for(int i = 0; i < 16; i++) {
+						dataTextFieldLista.get(i).setEditable(false);
+					}
+					btnComprobar.setVisible(false);
 				}
-				
-				for(int i = 0; i < dataTextFieldLista.size(); i++) {
-					if(i < 4 && matriz.verificarSumaFila(matrizRecibida, 0, matriz.arregloArmadoFilas[0])) {
-						dataTextFieldLista.get(i).setBackground(new Color(198, 222, 111));
-						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
-					}
-					else if(i < 8 && matriz.verificarSumaFila(matrizRecibida, 1, matriz.arregloArmadoFilas[1])){
-						dataTextFieldLista.get(i).setBackground(new Color(198, 222, 111));
-						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
-					}
-					else if(i < 12 && matriz.verificarSumaFila(matrizRecibida, 2, matriz.arregloArmadoFilas[2])){
-						dataTextFieldLista.get(i).setBackground(new Color(198, 222, 111));
-						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
-					}
-					else if(i < 16 && matriz.verificarSumaFila(matrizRecibida, 3, matriz.arregloArmadoFilas[3])){
+
+				//Asumimos que la matriz siempre sera de 4x4
+				//Chequeo la suma de las filas
+				for(int i = 0; i < 4; i++) {
+					if(matriz.verificarSumaFila(matrizRecibida, 0, nivelClase.cambiarNivelFila(nivel)[0])) {
 						dataTextFieldLista.get(i).setBackground(new Color(198, 222, 111));
 						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
 					}
 					else {
 						dataTextFieldLista.get(i).setBackground(new Color(227, 0, 82));
 						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
+						comprobar = false;
 					}
 				}
-//			Intento de mostrar en rojo si las columnas dan bien pero tira error
-				for(int i = 0; i < 4; i++) {
-					for(int j = 0; j < dataTextFieldLista.size(); j+=4) {
-					if(!(i == 0 && matriz.verificarSumaColumna(matrizRecibida, 0, matriz.arregloArmadoColumnas[0]))) {
-						dataTextFieldLista.get(j).setBackground(new Color(227, 0, 82));
-						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
-					}
-					
-					else if(!(i == 1 && matriz.verificarSumaColumna(matrizRecibida, 1, matriz.arregloArmadoColumnas[1]))){
-						dataTextFieldLista.get(j+1).setBackground(new Color(227, 0, 82));
-						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
-					}
-					
-					else if(!(i == 2 && matriz.verificarSumaColumna(matrizRecibida, 2, matriz.arregloArmadoColumnas[2]))){
-						dataTextFieldLista.get(j+2).setBackground(new Color(227, 0, 82));
-						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
-					}
-					
-					else if(!(i == 3 && matriz.verificarSumaColumna(matrizRecibida, 3, matriz.arregloArmadoColumnas[3]))){
-						dataTextFieldLista.get(j+3).setBackground(new Color(227, 0, 82));
+				for(int i = 4; i < 8; i++) {
+					if(matriz.verificarSumaFila(matrizRecibida, 1, nivelClase.cambiarNivelFila(nivel)[1])){
+						dataTextFieldLista.get(i).setBackground(new Color(198, 222, 111));
 						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
 					}
 					else {
-						dataTextFieldLista.get(i).setBackground(new Color(198, 222, 111));
+						dataTextFieldLista.get(i).setBackground(new Color(227, 0, 82));
+						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
+						comprobar = false;
 					}
 				}
+				for(int i = 8; i < 12; i++) {
+					if(matriz.verificarSumaFila(matrizRecibida, 2, nivelClase.cambiarNivelFila(nivel)[2])) {
+						dataTextFieldLista.get(i).setBackground(new Color(198, 222, 111));
+						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
+					}
+					else {
+						dataTextFieldLista.get(i).setBackground(new Color(227, 0, 82));
+						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
+						comprobar = false;
+					}
+
 				}
-				
-		}
-				
-				
-			
-			
+				for(int i = 12; i < 16; i++) {
+					if(matriz.verificarSumaFila(matrizRecibida, 3, nivelClase.cambiarNivelFila(nivel)[3])) {
+						dataTextFieldLista.get(i).setBackground(new Color(198, 222, 111));
+						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
+					}
+					else {
+						dataTextFieldLista.get(i).setBackground(new Color(227, 0, 82));
+						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
+						comprobar = false;
+					}
+				}
+
+				//Chequeo suma columnas
+
+				for(int i = 0; i < 16; i+=4) {
+					if(!matriz.verificarSumaColumna(matrizRecibida, 0, nivelClase.cambiarNivelColumna(nivel)[0])) {
+						dataTextFieldLista.get(i).setBackground(new Color(227, 0, 82));
+						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
+						comprobar = false;
+					}
+				}
+				for(int i = 1; i < 16; i+=4) {
+					if(!matriz.verificarSumaColumna(matrizRecibida, 1, nivelClase.cambiarNivelColumna(nivel)[1])){
+						dataTextFieldLista.get(i).setBackground(new Color(227, 0, 82));
+						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
+						comprobar = false;
+				}
+				}
+				for(int i = 2; i < 16; i+=4) {
+					if(!matriz.verificarSumaColumna(matrizRecibida, 2, nivelClase.cambiarNivelColumna(nivel)[2])) {
+						dataTextFieldLista.get(i).setBackground(new Color(227, 0, 82));
+						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
+						comprobar = false;
+					}
+
+				}
+				for(int i = 3; i < 16; i+=4) {
+					if(!matriz.verificarSumaColumna(matrizRecibida, 3, nivelClase.cambiarNivelColumna(nivel)[3])) {
+						dataTextFieldLista.get(i).setBackground(new Color(227, 0, 82));
+						dataTextFieldLista.get(i).setForeground(new Color(0, 0, 0));
+						comprobar = false;
+					}
+				}
+
+				if(!comprobar) {
+					btnReintentar.setVisible(true);
+				}
+				if(nivel !=6 && comprobar) {
+					nivel+=1;
+					btnSiguiente.setVisible(true);
+					btnReintentar.setVisible(false);
+					btnFinalizar.setVisible(false);
+				}
+				if(nivel == 6 && comprobar) {
+					btnFinalizar.setVisible(true);
+					btnReintentar.setVisible(false);
+				}
+				System.out.println(nivel);
+			}
 		});
 
-		frmJuegoAritmetico.getContentPane().add(btnComprobar);
 
 
+
+		btnReintentar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for(int i = 0; i < dataTextFieldLista.size(); i++) {
+					dataTextFieldLista.get(i).setText(null);
+					dataTextFieldLista.get(i).setBackground(new Color(53, 28, 77));
+					dataTextFieldLista.get(i).setForeground(new Color(251, 194, 252));
+				}
+
+				btnComprobar.setVisible(true);
+				for(int i = 0; i < 16; i++) {
+					dataTextFieldLista.get(i).setEditable(true);
+				}
+				btnReintentar.setVisible(false);
+			}
+		});
+		
+		//Acciones del BOTTON SIGUIENTE
+
+		btnSiguiente.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for(int i = 0; i < dataTextFieldLista.size(); i++) {
+					dataTextFieldLista.get(i).setText(null);
+					dataTextFieldLista.get(i).setBackground(new Color(53, 28, 77));
+					dataTextFieldLista.get(i).setForeground(new Color(251, 194, 252));
+				}
+				for(int i = 0; i < 16; i++) {
+					dataTextFieldLista.get(i).setEditable(true);
+				}
+				for(int i = 0; i < 4; i++) {
+					arregloSumaFila.get(i).setText(Integer.toString(nivelClase.cambiarNivelFila(nivel)[i]));
+					arregloSumaColumna.get(i).setText(Integer.toString(nivelClase.cambiarNivelColumna(nivel)[i]));
+				}
+				
+				btnComprobar.setVisible(true);
+				btnSiguiente.setVisible(false);
+			}
+		});
+		
+		//Acciones del BOTTON FINALIZAR
+
+		btnFinalizar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+					frmJuegoAritmetico.dispose();
+			}
+			
+		});
 	}
 }
